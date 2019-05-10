4a56ab4f07c325e84f13131b00f13a7b32729443
Leesoo Ahn <yisooan@fedoraproject.org>
2019-05-10 13:33:32 +0900

    vector: Refactor public APIs
    Since the commit, 2426d927932c5246569566b2c7f2a67b3d1c0ce2,
    vector header file has only public APIs.

--- include/dzf/dzf-vector.h
+++ include/dzf/dzf-vector.h
@@ -21,258 +21,126 @@
  * THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-/*! @file dzf-vector.h
- * 
- * @brief Vector data structure.
- * 
- * Vector is a data structure that the space of it is automatically able 
- * to extend the size of it when there is no space for a value to be added.
- * 
- * However, this is \b type-unsafe vector. functions are implemented based on 
- * \c #define directive. You must always take care of the type, \c T of dzf_vec_t(T).
- * 
- * When no space condition is met, the capacity would be multiplied by 2 for
- * every extension as below,
- * @code
- *   8 (with DFLT_VEC_CAP)
- *   16
- *   32
- *   64
- *   128
- *   256
- * @endcode
- * 
- * Please, take a look at dzf-vector.h and main.c in examples for how-to.
- */
-
 #ifndef __DZF_VEC_H__
 #define __DZF_VEC_H__
 
 #if !defined (__LIBDZF_H_INCLUDE__)
-#error "Only <dzf.h> can be included directly!"
+#   error "Only <dzf.h> can be included directly!"
 #endif
 
-
-#include "dzf-util.h"
-
-#define _dzf_vec_log(prefix, fmt, ...) \
-    _dzf_dbugf("** DZF::VECTOR::" prefix " -- ", fmt, __VA_ARGS__)
-
-#define DFLT_VEC_CAP    8   /* default capacity */
+#include "dzf-vector-private.h"
 
 
-
-/*
- * Attempt to allocate memory as much as size of T multiplies by capacity.
- * 
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @param _vec_cap: Capacity.
- * @return None
- */
-#define _dzf_vec_priv_init(_vecptr, _vec_cap) \
-    do { \
-        dzf_malloc((_vecptr)->data, dzf_sizeof(_vecptr) * _vec_cap); \
-        _dzf_vec_log("MALLOC", "Required size: %zd bytes.\n", \
-                dzf_sizeof(_vecptr) * _vec_cap); \
-        dzf_vec_size(_vecptr) = 0; \
-        dzf_vec_cap(_vecptr) = _vec_cap; \
-    } while(FALSE)
+__DZF_PUBLIC
+#define dzf_vec_new_with(_vecptr, _elem_size, _cap_sze) \
+    __dzf_vec_init(DZF_VEC_VOID(_vecptr), _elem_size, _cap_sze)
 
 
-/*
- * Attempt to resize alloc'd memory as much as capacity multiplies by 2.
- *
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @return None
- */
-#define _dzf_vec_priv_extend(_vecptr) \
-    ( dzf_vec_is_full(_vecptr) ? \
-      ( dzf_realloc( \
-            (_vecptr)->data, (_vecptr)->data, \
-            dzf_sizeof(_vecptr) * (dzf_vec_cap(_vecptr) * 2) ), \
-        _dzf_vec_log( /* Print how many it is realloc'd. */ \
-            "REALLOC", "Required size: %zd bytes.\n", \
-            dzf_sizeof(_vecptr) * (dzf_vec_cap(_vecptr) * 2) ), \
-        dzf_vec_cap(_vecptr) = dzf_vec_cap(_vecptr) * 2 \
-      ) : 0 )
+__DZF_PUBLIC
+#define dzf_vec_new(_vecptr, _elem_size) \
+    dzf_vec_new_with(_vecptr, _elem_size, DZF_VEC_DFLT_CAP)
 
 
-/*
- * Attempt to shrink alloc'd memory as much as divide by 2.
- *
- * Todo
- *
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @return None
- */
-#define _dzf_vec_priv_shrink(_vecptr)
-
-
-/*
- * Check whether the index is out of index or not.
- *
- * If it is out of index, exception of assert raises.
- *
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @return None
- */
-#define _dzf_vec_priv_idx_chk(_vecptr, _idx) \
-    assert(_idx >= 0 && _idx < dzf_vec_size(_vecptr))
-
-
-/*
- * Get/Set a value at the index without care of type.
- * 
- * This is as Getter and Setter.
- * 
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @return \c T typed data.
- */
-#define _dzf_vec_priv_at(_vecptr, _idx) \
-    ((_vecptr)->data[_idx])
-
-
-/*!
- * Initialize a vector with required capacity size.
- *
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @param _cap_sze: Capacity.
- * @return None
- */
-#define dzf_vec_new_with(_vecptr, _cap_sze) \
-    _dzf_vec_priv_init(_vecptr, _cap_sze)
-
-
-/*!
- * Initialize a vector with default capacity, 8 (DFLT_VEC_CAP).
- * 
- * @param _vecptr: A pointer to the dzf_vec_t(T)
- * @return None
- */
-#define dzf_vec_new(_vecptr) \
-    _dzf_vec_priv_init(_vecptr, DFLT_VEC_CAP)
-
-
-/*!
- * Free allocated memory to the data member.
- * 
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @return None
- */
+__DZF_PUBLIC
 #define dzf_vec_free(_vecptr) \
-    for ( free((_vecptr)->data), \
-          (_vecptr)->data = NULL; \
-          FALSE; )
+    ( assert(__dzf_vec_magic_validator(DZF_VEC_VOID(_vecptr))), \
+      __dzf_vec_free(DZF_VEC_VOID(_vecptr)) )
 
 
-/*!
- * Get how many elements it has.
- * 
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @return \c int
- */
-#define dzf_vec_size(_vecptr) \
-    ((_vecptr)->size)
+__DZF_PUBLIC
+#define dzf_vec_get_length(_vecptr) \
+    ( assert(__dzf_vec_magic_validator(DZF_VEC_VOID(_vecptr))), \
+      __dzf_vec_get_length(DZF_VEC_VOID(_vecptr)) )
 
 
-/*!
- * Get capacity of the vector.
- * 
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @return \c int
- */
-#define dzf_vec_cap(_vecptr) \
-    ((_vecptr)->_cap)
+__DZF_PUBLIC
+#define dzf_vec_get_capacity(_vecptr) \
+    ( assert(__dzf_vec_magic_validator(DZF_VEC_VOID(_vecptr))), \
+      __dzf_vec_get_capacity(DZF_VEC_VOID(_vecptr)) )
 
 
-/*!
- * Check whether it is full or not.
- * 
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @return TRUE if it is full, or FALSE.
- */
+__DZF_PUBLIC
 #define dzf_vec_is_full(_vecptr) \
-    ( dzf_vec_cap(_vecptr) == dzf_vec_size(_vecptr) ? TRUE : FALSE ) 
+    ( assert(__dzf_vec_magic_validator(DZF_VEC_VOID(_vecptr))), \
+      __dzf_vec_is_full(DZF_VEC_VOID(_vecptr)) )
 
 
-/*!
- * Return \c T typed data at the index.
- * 
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @param _idx: An index of the vector.
- * @return \c T typed data at the index.
- */
-#define dzf_vec_get_val_at(_vecptr, _idx) \
-    ( _dzf_vec_priv_idx_chk(_vecptr, _idx), _dzf_vec_priv_at(_vecptr, _idx) )
+__DZF_PUBLIC
+#define dzf_vec_is_empty(_vecptr) \
+    ( assert(__dzf_vec_magic_validator(DZF_VEC_VOID(_vecptr))), \
+      __dzf_vec_is_empty(DZF_VEC_VOID(_vecptr)) )
 
 
-/*!
- * Return a pointer to \c T typed data at the index.
- * 
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @param _idx: An index of the vector.
- * @return A pointer to \c T typed data at the index.
- */
-#define dzf_vec_get_ptr_at(_vecptr, _idx) \
-    ( _dzf_vec_priv_idx_chk(_vecptr, _idx), &_dzf_vec_priv_at(_vecptr, _idx) )
+__DZF_PUBLIC
+#define dzf_vec_get_pointer(_vecptr, _idx) \
+    dzf_vec_get_pointer_at(_vecptr, _idx)
 
 
-/*!
- * Set a new value at an index.
- * 
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @param _idx: An index of the vector.
- * @param value: \c T typed new value.
- * @return None
- */
-#define dzf_vec_set(_vecptr, _idx, value) \
-    for ( _dzf_vec_priv_idx_chk(_vecptr, _idx), \
-          _dzf_vec_priv_at(_vecptr, _idx) = value; \
-          FALSE; /* must be false not to loop. */ )
+__DZF_PUBLIC
+#define dzf_vec_get_pointer_at(_vecptr, _idx) \
+    ( assert(__dzf_vec_magic_validator(DZF_VEC_VOID(_vecptr))), \
+      assert(__dzf_vec_index_validator(DZF_VEC_VOID(_vecptr), _idx)), \
+      __dzf_vec_get_pointer_at(DZF_VEC_VOID(_vecptr), _idx) )
 
 
-/*!
- * Add a new value at the end of the vec_t(T).
- * 
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @param value: \c T typed value.
- * @return None
- */
-#define dzf_vec_add(_vecptr, value) \
-    for ( _dzf_vec_priv_extend(_vecptr), \
-          _dzf_vec_priv_at(_vecptr, dzf_vec_size(_vecptr)) = value, \
-          ++dzf_vec_size(_vecptr); \
-          FALSE; )
+__DZF_PUBLIC
+#define dzf_vec_set_value(_vecptr, _idx, _val) \
+    dzf_vec_set_value_at(_vecptr, _idx, _val)
 
 
-/*!
- * Remove an element at an index.
- * 
- * <i>Note that it takes O(N) time for left-shifting after remove it.</i>
- * 
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @param _idx: An index of the vector.
- * @return None
- */
-#define dzf_vec_rmv(_vecptr, _idx) \
-    for ( int i = (_dzf_vec_priv_idx_chk(_vecptr, _idx), _idx); \
-          (i < dzf_vec_size(_vecptr) - 1) \
-             ? TRUE : (--dzf_vec_size(_vecptr), FALSE); \
-          (_dzf_vec_priv_at(_vecptr, i) = _dzf_vec_priv_at(_vecptr, i+1)), ++i )
+__DZF_PUBLIC
+#define dzf_vec_set_value_at(_vecptr, _idx, _val) \
+    ( assert(__dzf_vec_magic_validator(DZF_VEC_VOID(_vecptr))), \
+      assert(__dzf_vec_index_validator(DZF_VEC_VOID(_vecptr), _idx)), \
+      __dzf_vec_set_value_at(_vecptr, _idx, _val) )
 
 
-/*!
- * Do something with the function through all elements.
- * 
- * @param _vecptr: A pointer to the dzf_vec_t(T).
- * @param _fptr: A pointer to a function that will be called.
- * @param ...: Various arguments to be passed into the function.
- * @return None
- */
+__DZF_PUBLIC
+#define dzf_vec_get_value(_vecptr, _idx) \
+    dzf_vec_get_value_at(_vecptr, _idx)
+
+
+__DZF_PUBLIC
+#define dzf_vec_get_value_at(_vecptr, _idx) \
+    ( assert(__dzf_vec_magic_validator(DZF_VEC_VOID(_vecptr))), \
+      assert(__dzf_vec_index_validator(DZF_VEC_VOID(_vecptr), _idx)), \
+      __dzf_vec_get_value_at(_vecptr, _idx) )
+
+
+__DZF_PUBLIC
+#define dzf_vec_add(_vecptr, _idx, _val) \
+    dzf_vec_add_at(_vecptr, _idx, _val)
+
+
+__DZF_PUBLIC
+#define dzf_vec_add_at(_vecptr, _idx, _val) \
+    DZF_GUARD(__dzf_vec_insert_at(_vecptr, _idx, _val))
+
+
+__DZF_PUBLIC
+#define dzf_vec_add_back(_vecptr, _val) \
+    dzf_vec_add_at(_vecptr, __dzf_vec_get_length(DZF_VEC_VOID(_vecptr)), _val)
+
+
+__DZF_PUBLIC
+#define dzf_vec_remove(_vecptr, _idx) \
+    dzf_vec_remove_at(_vecptr, _idx)
+
+
+__DZF_PUBLIC
+#define dzf_vec_remove_at(_vecptr, _idx) \
+    DZF_GUARD(__dzf_vec_remove_at(_vecptr, _idx))
+
+
+__DZF_PUBLIC
+#define dzf_vec_remove_back(_vecptr) \
+    dzf_vec_remove_at(_vecptr, __dzf_vec_get_length(_vecptr)-1)
+
+
 #define dzf_vec_foreach(_vecptr, _fptr, ...) \
     for ( int i = 0; \
-          i < dzf_vec_size(_vecptr); \
-          (_fptr)(&_dzf_vec_priv_at(_vecptr, i), __VA_ARGS__), ++i )
+          i < dzf_vec_get_length(_vecptr); \
+          (_fptr)(dzf_vec_get_pointer(DZF_VEC_VOID(_vecptr), i), __VA_ARGS__), ++i )
 
 
 #endif
