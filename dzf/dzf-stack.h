3cc667c6b4203c5f6c7dea78f0b509ad96b0c5ce
Leesoo Ahn <lsahn@ooseel.net>
2023-01-11 16:08:32 +0900

    dzf: update years of copyright to 2023

--- dzf/dzf-stack.h
+++ dzf/dzf-stack.h
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (c) 2018-2021 Leesoo Ahn <lsahn@ooseel.net>
+ * Copyright (c) 2018-2023 Leesoo Ahn <lsahn@ooseel.net>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
